bb37c9c32536f43f09ca3a00f0d3c416636c60c8
WoonU <nohog94@gmail.com>
2021-02-08 20:31:46 +0900

    2.0으로 변경

--- MiniProject_1.5/Form1.cs
+++ MiniProject_1.5/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,14 +20,16 @@ namespace MiniProject_1._5
         }
         /// //////////////////
         /// 전역 변수부
-        static byte[,] inImage, outImage;
+        static byte[,,] inImage = null, outImage = null;
         static int inH, inW, outH, outW;
         static string fileName;
-        static Bitmap paper; // 그림을 콕콕 찍을 종이
+        static Bitmap paper, bitmap; // 그림을 콕콕 찍을 종이
         static int upload_flag = 0; // 파일을 열면 1로 설정
         // 0 : bright, 1 : dark, 2 : reverse, 3: black white, 4 : gamma, 5: filter, 6: move, 7: upsidedown, 8: rotate, 9: zoom in, 10: zoom out
         static int[] change_flag = Enumerable.Repeat<int>(0, 11).ToArray<int>();       
         double[,] mask = new double[5, 5];
+
+        const int RGB = 3, RR = 0, GG = 1, BB = 2;
 
         private void 열기ToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -443,41 +446,50 @@ namespace MiniProject_1._5
         {
             upload_flag = 1;
             OpenFileDialog ofd = new OpenFileDialog();  // 객체 생성
+            ofd.Filter = "칼라 필터 | *.png; *.jpg; *.bmp; *.tif";
             if (ofd.ShowDialog() == DialogResult.Cancel)
             {
                 return;
             }
             fileName = ofd.FileName;
-
-            BinaryReader br = new BinaryReader(File.Open(fileName, FileMode.Open));
             // 파일 크기 알아내기 (?)
-            long fsize = new FileInfo(fileName).Length;
+            bitmap = new Bitmap(fileName);
             // 중요! 입력이미지의 높이, 폭 알아내기
-            inH = inW = (int)Math.Sqrt(fsize);
-            inImage = new byte[inH, inW]; // 메모리 할당
+            inW = bitmap.Height;
+            inH = bitmap.Width;
+            inImage = new byte[RGB, inH, inW]; // 메모리 할당
             for (int i = 0; i < inH; i++)
                 for (int k = 0; k < inW; k++)
-                    inImage[i, k] = br.ReadByte();
-            br.Close();
-
+                {
+                    Color c = bitmap.GetPixel(i, k);
+                    inImage[RR, i, k] = c.R;
+                    inImage[GG, i, k] = c.G;
+                    inImage[BB, i, k] = c.B;
+                }
             equal_image();
         }
 
         void saveImage()
         {
+
+            if (outImage == null)
+            {
+                MessageBox.Show("파일을 먼저 불러오세요");
+                return;
+            }
             SaveFileDialog sfd = new SaveFileDialog();      //파일 저장하는 위치선택창
             sfd.InitialDirectory = @"C:\images\RAW\saved_image";
-            sfd.DefaultExt = ".raw";
+            sfd.DefaultExt = ".jpg";
             if (sfd.ShowDialog() == DialogResult.Cancel)
             {
                 return;
             }
-            BinaryWriter bw = new BinaryWriter(new FileStream(sfd.FileName, FileMode.Create));
-            for (int i = 0; i < outH; i++)
-                for (int k = 0; k < outW; k++)
-                    bw.Write(outImage[i,k]);
-            bw.Close();
+            Bitmap img = new Bitmap(pictureBox1.Image);
+            
+            img.Save(sfd.FileName, ImageFormat.Jpeg);
+            img.Dispose();
         }
+
 
         void displayImage()
         {
@@ -488,8 +500,10 @@ namespace MiniProject_1._5
             for (int i = 0; i < outH; i++)
                 for (int k = 0; k < outW; k++)
                 {
-                    byte data = outImage[i, k]; // 잉크(색상값)
-                    pen = Color.FromArgb(data, data, data); // 펜에 잉크 묻히기
+                    byte r = outImage[RR, k, i]; // 잉크(색상값)
+                    byte g = outImage[GG, k, i]; // 잉크(색상값)
+                    byte b = outImage[BB, k, i]; // 잉크(색상값)
+                    pen = Color.FromArgb(r, g, b); // 펜에 잉크 묻히기
 
                     try
                     {
@@ -537,34 +551,37 @@ namespace MiniProject_1._5
             if (inImage == null)
                 return;
             outH = inH; outW = inW;
-            outImage = new byte[outH, outW];
-            for (int i = 0; i < inH; i++)
-                for (int k = 0; k < inW; k++)
-                {
-                    outImage[i, k] = inImage[i, k];
-                }
+            outImage = new byte[RGB, outH, outW];
+            for(int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < inH; i++)
+                    for (int k = 0; k < inW; k++)
+                    {
+                        outImage[rgb, i, k] = inImage[rgb, i, k];
+                    }
             displayImage();
         }
 
         void equal_image2()
         {
-            for (int i = 0; i < inH; i++)
-                for (int k = 0; k < inW; k++)
-                {
-                    inImage[i, k] = outImage[i, k];
-                }
+            for (int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < inH; i++)
+                    for (int k = 0; k < inW; k++)
+                    {
+                        inImage[rgb, i, k] = outImage[rgb, i, k];
+                    }
         }
 
         void bright(int color)
         {
-            for (int i = 0; i < inH; i++)
-                for (int k = 0; k < inW; k++)
-                {
-                    if ((inImage[i, k] + color) > 255)
-                        outImage[i, k] = 255;
-                    else
-                        outImage[i, k] = (byte)((int)inImage[i, k] + color);
-                }
+            for (int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < inH; i++)
+                    for (int k = 0; k < inW; k++)
+                    {
+                        if ((inImage[rgb, i, k] + color) > 255)
+                            outImage[rgb, i, k] = 255;
+                        else
+                            outImage[rgb, i, k] = (byte)((int)inImage[rgb, i, k] + color);
+                    }
             displayImage();
             equal_image2();
         }
@@ -572,14 +589,15 @@ namespace MiniProject_1._5
 
         void dark(int color)
         {
-            for (int i = 0; i < inH; i++)
-                for (int k = 0; k < inW; k++)
-                {
-                    if ((inImage[i, k] - color) < 0)
-                        outImage[i, k] = 0;
-                    else
-                        outImage[i, k] = (byte)((int)inImage[i, k] - color);
-                }
+            for (int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < inH; i++)
+                    for (int k = 0; k < inW; k++)
+                    {
+                        if ((inImage[rgb, i, k] - color) < 0)
+                            outImage[rgb, i, k] = 0;
+                        else
+                            outImage[rgb, i, k] = (byte)((int)inImage[rgb, i, k] - color);
+                    }
             displayImage();
             equal_image2();
         }
@@ -588,13 +606,14 @@ namespace MiniProject_1._5
 
         void reverse()
         {
-            for (int i = 0; i < inH; i++)
-            {
-                for (int j = 0; j < inW; j++)
+            for (int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < inH; i++)
                 {
-                    outImage[i, j] = (byte)(255 - (int)inImage[i, j]);
+                    for (int j = 0; j < inW; j++)
+                    {
+                        outImage[rgb, i, j] = (byte)(255 - (int)inImage[rgb, i, j]);
+                    }
                 }
-            }
             displayImage();
             equal_image2();
         }
@@ -603,31 +622,33 @@ namespace MiniProject_1._5
 
         void black_and_white()
         {
-            for (int i = 0; i < inH; i++)
-                for (int k = 0; k < inW; k++)
-                {
-                    if (inImage[i, k] > 128)
-                        outImage[i, k] = 255;
-                    else
-                        outImage[i, k] = 0;
-                }
+            for (int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < inH; i++)
+                    for (int k = 0; k < inW; k++)
+                    {
+                        if (inImage[rgb, i, k] > 128)
+                            outImage[rgb, i, k] = 255;
+                        else
+                            outImage[rgb, i, k] = 0;
+                    }
             displayImage();
             equal_image2();
         }
 
         void gamma(double gamma)
         {
-            for (int i = 0; i < inH; i++)
-            {
-                for (int j = 0; j < inW; j++)
+            for (int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < inH; i++)
                 {
-                    outImage[i, j] = (byte)((int)255 * Math.Pow(((double)inImage[i, j] / 255), gamma));
-                    if (outImage[i, j] > 255)
+                    for (int j = 0; j < inW; j++)
                     {
-                        outImage[i, j] = 255;
+                        outImage[rgb, i, j] = (byte)((int)255 * Math.Pow(((double)inImage[rgb, i, j] / 255), gamma));
+                        if (outImage[rgb, i, j] > 255)
+                        {
+                            outImage[rgb, i, j] = 255;
+                        }
                     }
                 }
-            }
             displayImage();
             equal_image2();
         }
@@ -636,50 +657,67 @@ namespace MiniProject_1._5
         void filter_image(double[,] mask)
         {
             double sum = 0;
-            for (int i = 0; i < inH - 4; i++)
-            {
-                for (int j = 0; j < inW - 4; j++)
-                {
-                    /* 기존 image와  filter에 대해서 convolution 수행 */
-                    for (int k = 0; k < 5; k++)
+            byte[,,] temp_inImage = new byte[RGB, inH+4, inW+4];
+            for (int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < inH+4; i++)
+                    for (int j = 0; j < inW+4; j++)
                     {
-                        for (int l = 0; l < 5; l++)
-                        {
-                            /* 결과를 sum에 저장 */
-                            sum += inImage[i + k, j + l] * mask[k, l];
-                        }
+                        temp_inImage[rgb, i, j] = 0;
                     }
-                    if (sum > 255)
-                        outImage[i, j] = 255;
-                    else if (sum < 0)
-                        outImage[i, j] = 0;
-                    else
-                        outImage[i, j] = (byte)sum;
-                    /* 다음 계산을 위해 sum 초기화 */
-                    sum = 0;
+            for (int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < inH; i++)
+                    for (int j = 0; j < inW; j++)
+                    {
+                        temp_inImage[rgb, i + 2, j + 2] = inImage[rgb, i, j];
+                    }
+                        
+            for (int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < inH; i++)
+                {
+                    for (int j = 0; j < inW; j++)
+                    {
+                        /* 기존 image와  filter에 대해서 convolution 수행 */
+                        for (int k = 0; k < 5; k++)
+                        {
+                            for (int l = 0; l < 5; l++)
+                            {
+                                /* 결과를 sum에 저장 */
+                                sum += temp_inImage[rgb, i + k, j + l] * mask[k, l];
+                            }
+                        }
+                        if (sum > 255)
+                            outImage[rgb, i, j] = 255;
+                        else if (sum < 0)
+                            outImage[rgb, i, j] = 0;
+                        else
+                            outImage[rgb, i, j] = (byte)sum;
+                        /* 다음 계산을 위해 sum 초기화 */
+                        sum = 0;
+                    }
                 }
-            }
             displayImage();
             equal_image2();
         }
 
         void move(int move_x, int move_y)
         {
-            for (int i = 0; i < inH; i++)
-                for (int j = 0; j < inW; j++)
-                    /* outImage 초기화 수행 */
-                    outImage[i, j] = 0;
-            for (int i = 0; i < inH; i++)
-            {
-                for (int j = 0; j < inW; j++)
+            for (int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < inH; i++)
+                    for (int j = 0; j < inW; j++)
+                        /* outImage 초기화 수행 */
+                        outImage[rgb, i, j] = 0;
+            for (int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < inH; i++)
                 {
-                    /* 경계를 벗어나지 않는 영역에 대해서만 outImage값 입력 */
-                    if (i + move_y < inH && j + move_y < inW)
-                        outImage[i + move_y, j + move_x] = inImage[i, j];
-                    else
-                        continue;
+                    for (int j = 0; j < inW; j++)
+                    {
+                        /* 경계를 벗어나지 않는 영역에 대해서만 outImage값 입력 */
+                        if (i + move_y < inH && j + move_y < inW)
+                            outImage[rgb, i + move_y, j + move_x] = inImage[rgb, i, j];
+                        else
+                            continue;
+                    }
                 }
-            }
             displayImage();
             equal_image2();
         }
@@ -688,13 +726,14 @@ namespace MiniProject_1._5
 
         void up_side_down()
         {
-            for (int i = 0; i < inH; i++)
-            {
-                for (int j = 0; j < inW; j++)
+            for (int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < inH; i++)
                 {
-                    outImage[i, j] = inImage[inH - 1 - i, j];
+                    for (int j = 0; j < inW; j++)
+                    {
+                        outImage[rgb, i, j] = inImage[rgb, inH - 1 - i, j];
+                    }
                 }
-            }
             displayImage();
             equal_image2();
         }
@@ -703,27 +742,29 @@ namespace MiniProject_1._5
         {
             float temp_x, temp_y;
             int temp_after_rotate_x, temp_after_rotate_y;
-            for (int i = 0; i < inH; i++)
-                for (int j = 0; j < inW; j++)
-                    outImage[i, j] = 0;
-            for (int i = 0; i < inH; i++)
-            {
-                for (int j = 0; j < inW; j++)
+            for (int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < inH; i++)
+                    for (int j = 0; j < inW; j++)
+                        outImage[rgb, i, j] = 0;
+            for (int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < inH; i++)
                 {
-                    /* 회전변환을 위해 원점을 inW/2, inH/2로 가정 */
-                    temp_x = j - inW / 2;
-                    temp_y = inH / 2 - i;
-                    /* 첨부한 그림 참조 */
-                    temp_after_rotate_x = (int)(Math.Cos(angle * Math.PI / 180) * temp_x + Math.Sin(angle * Math.PI / 180) * temp_y);
-                    temp_after_rotate_y = (int)(-Math.Sin(angle * Math.PI / 180) * temp_x + Math.Cos(angle * Math.PI / 180) * temp_y);
-                    if (temp_after_rotate_x < inH / 2 && temp_after_rotate_x > -inH / 2 &&
-                        temp_after_rotate_y < inW / 2 && temp_after_rotate_y > -inW / 2)
-                        /* 영역을 벗어나지 않은 부분에 대해서만 값 입력 */
-                        outImage[i, j] = inImage[inH / 2 - temp_after_rotate_y, temp_after_rotate_x + inW / 2];
-                    else
-                        continue;
+                    for (int j = 0; j < inW; j++)
+                    {
+                        /* 회전변환을 위해 원점을 inW/2, inH/2로 가정 */
+                        temp_x = j - inW / 2;
+                        temp_y = inH / 2 - i;
+                        /* 첨부한 그림 참조 */
+                        temp_after_rotate_x = (int)(Math.Cos(angle * Math.PI / 180) * temp_x + Math.Sin(angle * Math.PI / 180) * temp_y);
+                        temp_after_rotate_y = (int)(-Math.Sin(angle * Math.PI / 180) * temp_x + Math.Cos(angle * Math.PI / 180) * temp_y);
+                        if (temp_after_rotate_x < inH / 2 && temp_after_rotate_x > -inH / 2 &&
+                            temp_after_rotate_y < inW / 2 && temp_after_rotate_y > -inW / 2)
+                            /* 영역을 벗어나지 않은 부분에 대해서만 값 입력 */
+                            outImage[rgb, i, j] = inImage[rgb, inH / 2 - temp_after_rotate_y, temp_after_rotate_x + inW / 2];
+                        else
+                            continue;
+                    }
                 }
-            }
             displayImage();
             equal_image2();
         }
@@ -731,12 +772,13 @@ namespace MiniProject_1._5
         void zoom_in(double scale)
         {
             outH = (int)(inH * scale); outW = (int)(inW * scale);
-            outImage = new byte[outH, outW];
-            for (int i = 0; i < outH; i++)
-                for (int k = 0; k < outW; k++)
-                {
-                    outImage[i, k] = (inImage[(byte)(i / scale), (byte)(k / scale)]);
-                }
+            outImage = new byte[RGB, outH, outW];
+            for (int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < outH; i++)
+                    for (int k = 0; k < outW; k++)
+                    {
+                        outImage[rgb, i, k] = (inImage[rgb, (byte)(i / scale), (byte)(k / scale)]);
+                    }
             displayImage();
             equal_image2();
         }
@@ -744,12 +786,13 @@ namespace MiniProject_1._5
         void zoom_out(double scale)
         {
             outH = (int)(inH / scale); outW = (int)(inW / scale);
-            outImage = new byte[outH, outW];
-            for (int i = 0; i < outH; i++)
-                for (int k = 0; k < outW; k++)
-                {
-                    outImage[i, k] = inImage[i * (byte)scale, k * (byte)scale];
-                }
+            outImage = new byte[RGB, outH, outW];
+            for (int rgb = 0; rgb < RGB; rgb++)
+                for (int i = 0; i < outH; i++)
+                    for (int k = 0; k < outW; k++)
+                    {
+                        outImage[rgb, i, k] = inImage[rgb, i * (byte)scale, k * (byte)scale];
+                    }
             displayImage();
         }
     }
